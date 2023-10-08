aea945610d805e06e7ab9d3278265dd6308d27d1
Fabio Vige <fabiovige@gmail.com>
2023-10-08 15:29:40 -0300

    tratamento de erros, async await, try cat

--- src/main.js
+++ src/main.js
@@ -1,1 +1,40 @@
-console.log("Hello Worl TESTE!");
+import * as calculator from './calculator.js';
+import soma from './sum.js';
+
+console.log(calculator.sum(16, 12));
+console.log(calculator.subtraction(13, 2));
+
+console.log(soma(16, 12));
+
+
+const myPromise = new Promise((resolve, reject) => {
+  return setTimeout(() => {
+    resolve('OK deu erro ... ');
+  }, 1000);
+});
+
+const exec = async () => {
+  try {
+    loading = true
+    await myPromise;
+    console.log('teste 1...');
+
+    await myPromise;
+    console.log('teste 2...');
+  } catch (error) {
+    console.warn(error);
+  } finally {
+    loading = false
+    console.log('finally');
+  }
+
+}
+
+exec();
+
+// myPromise.then(() => {
+//   console.log('deu certo');
+// }).catch((error) => {
+//   console.warn(error);
+// });
+
